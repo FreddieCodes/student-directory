f1e374c3a0b0031d7724af3238c2f024d1a8a8d0
FreddieCodes <FrederickCodes@gmail.com>
2017-07-21 00:21:48 +0000

    Step 12 (Loading data from the file)

--- directory.rb
+++ directory.rb
@@ -52,6 +52,8 @@ def process(selection)
         show_students
       when "3"
         save_students
+      when "4"
+        load_students
       when "9"
         exit # causes the program to terminate
       else
@@ -63,6 +65,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -108,6 +111,18 @@ def save_students
     end
     file.close
 end
+
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, cohort = line.chomp.split(",")
+        @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+end
+
+
+
 # nothing happends until we call the method
 interactive_menu
 
